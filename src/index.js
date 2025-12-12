b7e163ce4072bd833f0be819262c42f89daf9030
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-12 20:27:22 +0530

    chore: setup server startup with DB connection and global error handling

--- src/index.js
+++ src/index.js
@@ -2,5 +2,49 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config({path : './.env'})
 import connectDB from './db/index.js'
+import {app} from './app.js'
+
+
+const PORT = process.env.PORT || 8000;
+
+// handle error 
+app.on('error' , (err) => {
+    console.log("SERVER ERROR:", err);
+    process.exit(1); // graceful shutdown the server
+})
 
 connectDB()
+.then(() => {
+    app.listen(PORT , () => {
+        console.log(`App is listening on port ${PORT}`);
+    })
+})
+.catch((error) => {
+    console.log(`DATABASE CONNECTION FAILED ${error}`)
+})
+
+
+
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("SERVER ERROR: ", error);
+            process.exit(1);
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
