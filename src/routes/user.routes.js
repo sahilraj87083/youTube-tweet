1ecb221e5accafb0e428fd4c453c1287ff593f08
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-14 20:05:28 +0530

    update account route configured

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -26,6 +26,7 @@ router.route('/logout').post(verifyJWT, logoutUser)
 router.route('/refresh-token').post(refreshAccessToken)
 router.route('/change-password').post(verifyJWT, changeCurrentPassword)
 router.route('/current-user').post(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 
 export default router;
