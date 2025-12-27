dbda0f180146e5a99ab5c249a9868e49a4deec50
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-27 14:25:16 +0530

    feat: add healthcheck endpoint

---
+++ backend/src/controllers/healthcheck.controller.js
@@ -0,0 +1,14 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import { Tweet } from "../models/tweet.models.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import mongoose, { isValidObjectId } from "mongoose";
+import { User } from "../models/user.models.js";
+
+const healthcheck = asyncHandler(async (req, res) => {
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { message: "Everything is O.K" }, "Ok"));
+});
+
+export { healthcheck };
